44c7f87db159ab62ffd2f5e4f6bc69cf400de3c1
Leismael <leismael.sosa17@gmail.com>
2022-08-17 04:38:58 -0400

    Add the bag.pl file with the 'new_bag' predicate

---
+++ src/bag.pl
@@ -0,0 +1,16 @@
+:- [utils].
+
+% Este predicado triunfa si X es un azulejo valido
+tiles(X) :-
+    member(X, [azul, amarillo, rojo, negro, cyan]).
+
+% ----------------------------------------------------------------------------------------
+
+% Triunfa si X es un bag nuevo, es decir, que contiene todas las fichas.
+% En caso de X ser una variable unbounded pues le asigna un bag nuevo
+new_bag(X) :-
+    findall(Tile:20, tiles(Tile), Tiles),
+    expanded(Tiles, Expanded),
+    random_permutation(Expanded, X).
+
+% ----------------------------------------------------------------------------------------
