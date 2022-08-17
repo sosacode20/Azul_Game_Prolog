2b814e08f79895f9c48e237d37f08c36a2ae1f1b
Leismael <leismael.sosa17@gmail.com>
2022-08-17 14:40:08 -0400

    Added the predicate 'add_to_center' for adding tiles from a factory to it

--- src/center.pl
+++ src/center.pl
@@ -2,3 +2,10 @@
 
 create_center(Center) :-
     Center = [first:1].
+
+% ----------------------------------------------------------------
+
+% New_tiles_to_add es un grupo, es decir, una lista donde sus elementos
+% tienen el formato: Element:Amount
+add_to_center(Center, New_tiles_to_add, New_center) :-
+    merge_groups(New_tiles_to_add, Center, New_center).
