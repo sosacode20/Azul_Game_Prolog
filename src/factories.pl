d4c7af4db5153ded795b109b160198055924b2c9
Leismael <leismael.sosa17@gmail.com>
2022-08-24 01:09:57 -0400

    Fixing the syntax of the predicate 'new_factories'
    The change of the syntax is because is necessary to take in account the 'Tape'
    in the specific case that with the tiles of the 'Bag' we cannot fullfil the tiles
    of the new 'factories' and we need to refill the 'Bag' with some tiles of the 'Tape'

--- src/factories.pl
+++ src/factories.pl
@@ -5,10 +5,21 @@ factory_player_relation(Players_count, Factory_count):-
     Players_count inside (2:4), % Ver el predicado 'inside' en Utils
     Factory_count is Players_count * 2 + 1.
 
-new_factories(Bag, Factories, Updated_bag) :-
+new_factories(Bag, Tape, Factories, Updated_bag, Updated_tape) :-
     get_players_count(Count),
     factory_player_relation(Count, Factory_count),
-    new_factories_(Bag, Factory_count, Factories, Updated_bag),!.
+    Needed_tiles is Factory_count * 4,
+    length(Bag, Length),
+    Amount_to_extract_from_tape is Needed_tiles - Length,
+    (
+        Amount_to_extract_from_tape > 0,
+        split(Amount_to_extract_from_tape, Tape, Extracted_amount, Updated_tape),
+        append(Bag, Extracted_amount, New_bag),
+        new_factories_(New_bag, Factory_count, Factories, Updated_bag),!;
+        % Aqui el Amount a extraer tiene que ser menor o igual a 0
+        new_factories_(Bag, Factory_count, Factories, Updated_bag),!
+    ).
+
 
 new_factories_(Bag, 1, [Filled_factory], Updated_bag):-
     extract_from_bag(Bag, 4, Extracted_tiles, Updated_bag),
